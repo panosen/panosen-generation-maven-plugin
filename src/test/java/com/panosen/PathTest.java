66267c33d53a52f41c9b93cacbbb4ebd76b9dd7b
harriszhang <harriszhang@live.cn>
2022-04-30 23:56:41 +0800

    fix UT

--- src/test/java/com/panosen/PathTest.java
+++ src/test/java/com/panosen/PathTest.java
@@ -12,12 +12,12 @@ public class PathTest {
     public void test() {
         Path path = Paths.get("a", "b", "1.txt");
 
+        String fileSeparator = System.getProperty("file.separator");
+
         String actual = path.toString();
-        String expected = "a\\b\\1.txt";
+        String expected = "a" + fileSeparator + "b" + fileSeparator + "1.txt";
 
         Assert.assertEquals(expected, actual);
-
-        Assert.assertEquals("\\", System.getProperty("file.separator"));
     }
 
 }
